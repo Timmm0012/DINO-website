b62933033ba8d5ba102e46e262fe7a033720e95e
timmm0012 <timklaassen1612@gmail.com>
2025-11-14 13:23:10 +0100

    FEAT: Added PHP Mailer

--- php/contact.php
+++ php/contact.php
@@ -1,3 +1,32 @@
-<?php 
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-?>
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+var_dump($_POST);
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'timklaassen1612@@gmail.com';
+    $mail->Password   = 'xvye ewkw kaix nyzh';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+
+    $mail->setFrom($_POST['email'], $_POST['firstname']);
+    $mail->addAddress('timklaassen1612@gmail.com');
+
+    $mail->Subject = "Nieuw bericht van " . $_POST['firstname'] . " " . $_POST['lastname'];
+    $mail->Body    = $_POST['message'];
+
+    $mail->send();
+    echo "Mail verstuurd!";
+} catch (Exception $e) {
+    echo "Mailer Error: {$mail->ErrorInfo}";
+}
